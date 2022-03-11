ef03ccb7a1c5737accbd6ea8073cc03a118435d3
Evan <evan@Evans-MBP.lan>
2022-03-11 16:15:01 -0500

    added data constants stub

---
+++ src/DataConstants.java
@@ -0,0 +1,32 @@
+/**
+ * DataConstants
+ * @author Evan Scales
+ */
+public abstract class DataConstants {
+    // Flights
+    protected static final String FLIGHTS_FILE_NAME = "json/flights.json";
+
+    // Friends
+    protected static final String FRIENDS_FILE_NAME = "json/friends.json";
+
+    // Hotels
+    protected static final String HOTELS_FILE_NAME = "json/hotels.json";
+
+    // Reservations
+    protected static final String RESERVATIONS_FILE_NAME = "json/reservations.json";
+
+    // Reviews
+    protected static final String REVIEWS_FILE_NAME = "json/reviews.json";
+
+    // Rooms
+    protected static final String ROOMS_FILE_NAME = "json/rooms.json";
+
+    // Seatings
+    protected static final String SEATINGS_FILE_NAME = "json/seatings.json";
+
+    // Tickets
+    protected static final String TICKETS_FILE_NAME = "json/tickets.json";
+
+    // Users
+    protected static final String USERS_FILE_NAME = "json/users.json";
+}
