769bdff9b1de30cfc84b95a9e6f2c9aa0a9f6d41
finn-cork <fcork@email.sc.edu>
2022-03-15 21:15:15 -0400

    Finished FlightSystem stub

---
+++ src/FlightSystem.java
@@ -0,0 +1,62 @@
+/**
+ * FlightSystem
+ * @author Lyn Cork
+ */
+public class FlightSystem {
+  private FlightList flightList;
+  private UserList userList;
+  private HotelList hotelList;
+  private User user;
+/**
+ * 
+ */
+  public FlightSystem() {
+    
+  }
+/**
+ * 
+ * @param username
+ * @param password
+ * @return
+ */
+  public User login(String username, String password) {
+    
+  }
+/**
+ * 
+ * @return
+ */
+  public User createAccount() {
+    
+  }
+/**
+ * 
+ */
+  public void viewAccount() {
+    
+  }
+/**
+ * 
+ */
+  public void logout() {
+    
+  }
+/**
+ * 
+ */
+  public void searchForFlights() {
+    
+  }
+/**
+ * 
+ */
+  public void searchForHotels() {
+    
+  }
+/**
+ * 
+ */
+  public void editPrefernces() {
+    
+  }
+}
