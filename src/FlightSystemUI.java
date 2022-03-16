d897beb89ff6b9edb1fde7771e7243092133c545
finn-cork <fcork@email.sc.edu>
2022-03-15 21:08:45 -0400

    Finished FlightSystemUI stub

---
+++ src/FlightSystemUI.java
@@ -0,0 +1,33 @@
+/**
+ * FlightSystemUI
+ * @author Lyn Cork
+ */
+import java.util.*;
+public class FlightSystemUI {
+  private Scanner scanner;
+  private FlightSystem system;
+/**
+ * 
+ */
+  public void run() {
+    
+  }
+/**
+ * 
+ */
+  private void displayMenuUser() {
+    
+  }
+/**
+ * 
+ */
+  private void displayMenuRegisteredUser() {
+    
+  }
+/**
+ * 
+ */
+  private void displayMenuAdmin() {
+    
+  }
+}
