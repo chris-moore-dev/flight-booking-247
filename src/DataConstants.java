f13f35eeb5178c8ce99a845138c45f0922ea528e
Evan <evan@Evans-MacBook-Pro.local>
2022-03-17 11:40:31 -0400

    added flight data constants

--- src/DataConstants.java
+++ src/DataConstants.java
@@ -5,6 +5,20 @@
 public abstract class DataConstants {
     // Flights
     protected static final String FLIGHTS_FILE_NAME = "json/flights.json";
+    protected static final String FLIGHTS_ID = "flightID";
+    protected static final String FLIGHTS_COMPANY = "company";
+    protected static final String FLIGHTS_DATE = "date";
+    protected static final String FLIGHTS_DEPARTING_AIRPORT = "departingAirport";
+    protected static final String FLIGHTS_DEST_AIRPORT = "destAirport";
+    protected static final String FLIGHTS_TAKE_OFF_TIME = "takeOffTime";
+    protected static final String FLIGHTS_LANDING_TIME = "landingTime";
+    protected static final String FLIGHTS_TOTAL_TIME = "totalFlightTime";
+    protected static final String FLIGHTS_LAYOVER = "layover";
+    protected static final String FLIGHTS_FLIGHT_LIST = "flights";
+    protected static final String FLIGHTS_NUM_STOPS = "numStops";
+    protected static final String FLIGHTS_PRICING_LIST = "pricing";
+    protected static final String FLIGHTS_INDIVIDUALBOOKINGS_LIST = "individualBookings";
+
 
     // Friends
     protected static final String FRIENDS_FILE_NAME = "json/friends.json";
