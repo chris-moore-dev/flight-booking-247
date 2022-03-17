cb3af9b7d574437c92263fe4ab23fc59269d8a88
Chris Moore <tonio64286@gmail.com>
2022-03-17 18:09:04 -0400

    Javadoc room.java stub

--- src/Room.java
+++ src/Room.java
@@ -2,39 +2,75 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Date;
 
+/**
+ * Room class
+ * @author
+ */
 public class Room extends IndividualBooking {
   private boolean smokingAllowed;
   private ArrayList<String> bookedDays;
   private int numBeds;
 
+  /**
+   * Default constructor
+   * @param smokingAllowed
+   * @param availability
+   */
   Room(Boolean smokingAllowed, HashMap<Date, Boolean> availability) {
     // super()
   }
 
   // GETTERS
 
+  /**
+   * 
+   * @param room
+   * @return
+   */
   public boolean getIsSmokingAllowed(Room room) {
     return this.smokingAllowed;
   }
 
+  /**
+   * 
+   * @param room
+   * @return
+   */
   public ArrayList<String> getBookedDays(Room room) {
     return this.bookedDays;
   }
 
+  /**
+   * 
+   * @param room
+   * @return
+   */
   public int getNumBeds(Room room) {
     return this.numBeds;
   }
 
   // SETTERS
 
+  /**
+   * 
+   * @param smokingAllowed
+   */
   public void setIsSmokingAllowed(boolean smokingAllowed) {
     this.smokingAllowed = smokingAllowed;
   }
 
+  /**
+   * 
+   * @param bookedDays
+   */
   public void setBookedDays(ArrayList<String> bookedDays) {
     this.bookedDays = bookedDays;
   }
 
+  /**
+   * 
+   * @param numBeds
+   */
   public void setNumBeds(int numBeds) {
     this.numBeds = numBeds;
   }
