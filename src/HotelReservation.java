ee6145c6da94cfc2056aee84b257155ecdffb8bf
finn-cork <fcork@email.sc.edu>
2022-03-24 13:47:12 -0400

    finished constructors for HotelReservation

--- src/HotelReservation.java
+++ src/HotelReservation.java
@@ -54,6 +54,7 @@ public class HotelReservation extends Reservation {
         setCheckInDate(checkInDate);
         setCheckOutDate(checkOutDate);
         setNumGuests(numGuests);
+        this.id = id;
     }
 
     // Member functions
