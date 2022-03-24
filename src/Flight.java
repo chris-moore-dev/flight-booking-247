0eb7418a721444431038a69ed5d4ffc564123991
Evan <escales@email.sc.edu>
2022-03-24 18:34:58 -0400

    succesfully tested flight list

--- src/Flight.java
+++ src/Flight.java
@@ -1,12 +1,11 @@
 /**
  * Flight
- * @author Chris Moore
+ * @author Chris Moore, Evan Scales
  */
 import java.util.HashMap;
 import java.util.UUID;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 public class Flight extends ObjectToBeBooked {
   private LocalDate date;
   private String departingAirport;
@@ -44,7 +43,17 @@ public class Flight extends ObjectToBeBooked {
   String takeOffTime, String landingTime, String totalFlightTime,
   boolean layover, String company, HashMap<String, Integer> pricing,
   HashMap<String, Seating> seats, String departingGate, String destGate) {
-    //super
+    super(company, pricing);
+    setIsLayover(layover);
+    setDate(date);
+    setDepartingAirport(departingAirport);
+    setDestAirport(destAirport);
+    setTakeOffTime(takeOffTime);
+    setLandingTime(landingTime);
+    setTotalFlightTime(totalFlightTime);
+    setSeats(seats);
+    setDapartingGate(departingGate);
+    setDestGate(destGate);
   }
 
   /**
@@ -67,7 +76,17 @@ public class Flight extends ObjectToBeBooked {
   String takeOffTime, String landingTime, String totalFlightTime,
   boolean layover, String company, HashMap<String, Integer> pricing,
   HashMap<String, Seating> seats, UUID id, String departingGate, String destGate) {
-    //super
+    super(company, pricing, id);
+    setIsLayover(layover);
+    setDate(date);
+    setDepartingAirport(departingAirport);
+    setDestAirport(destAirport);
+    setTakeOffTime(takeOffTime);
+    setLandingTime(landingTime);
+    setTotalFlightTime(totalFlightTime);
+    setSeats(seats);
+    setDapartingGate(departingGate);
+    setDestGate(destGate);
   }
 
   /**
@@ -338,13 +357,12 @@ public class Flight extends ObjectToBeBooked {
    * @param seats
    */
   public void setSeats(HashMap<String, Seating> seats) {
-    if (seats != null)
-      this.seats = seats;
-    else if (!layover)
+    if (seats == null)
+      this.seats = null;
+    else if(seats.isEmpty())
       this.seats = makeSeats();
     else
-      this.seats = null;
-
+      this.seats = seats;
   }
 
   /**
--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * test
@@ -9,6 +10,9 @@ import java.time.LocalDate;
 public class test {
 
     public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+
         FlightList flightList = FlightList.getInstance();
         HotelList hotelList = HotelList.getInstance();
         UserList userList = UserList.getInstance();
@@ -17,11 +21,21 @@ public class test {
         ArrayList<Hotel> hotels = hotelList.getHotels();
         ArrayList<RegisteredUser> users = userList.getUsers();
 
+        // RegisteredUser user = users.get(0);
+        // Flight flight = flights.get(0);
+        // String date = "09/11/2002";
+        // System.out.println(date);
+        // LocalDate date2 = LocalDate.parse(date, formatter);
+        // System.out.println(date2);
+
+        
+
         // String toSplit = "Standard:45";
         // String[] split = toSplit.split(":");
         // System.out.println(split[0]);
         // System.out.println(split[1]);
         testUserList(users.get(0));
+        testFlightList(flights.get(0));
         System.out.println(UUID.randomUUID());
     }
 
@@ -32,7 +46,8 @@ public class test {
      * Loading users with friends: PASS
      * Loading users tickets:
      * Loading users with reservations: 
-     * @pram user The users being tested
+     * @param user The users being tested
+     * @TEST
      */
     public static void testUserList(RegisteredUser user) {
         // Loading users by itself
@@ -51,6 +66,41 @@ public class test {
         for (Friend friend : friends) {
             System.out.println(friend);
         }
+    }
+
+    /**
+     * Basic loading of the flight (name, date, etc): PASS
+     * Loading of pricing hashmap: PASS
+     * Loading of seating hashmap: Pass
+     * @param flight The flight being tested
+     * @TEST
+     */
+    public static void testFlightList(Flight flight) {
+        System.out.println("-----TESTING FLIGHT-----");
+        System.out.println(flight.getID());
+        System.out.println(flight.getCompany());
+        System.out.println(flight.getDate());
+        System.out.println(flight.getDepartingAirport());
+        System.out.println(flight.getDestAirport());
+        System.out.println(flight.getTakeOffTime());
+        System.out.println(flight.getLandingTime());
+        System.out.println(flight.getTotalFlightTime());
+        System.out.println(flight.getIsLayover());
+        System.out.println(flight.getDepartingGate());
+        
+
+        System.out.println(flight.getPrice("First"));
+        System.out.println(flight.getPrice("Main Cabin"));
+        System.out.println(flight.getPrice("Economy"));
+
+        HashMap<String, Seating> seats = flight.getSeating();
+
+
+        System.out.println(seats.get("D1").getIsMedicalSeat());
+
+        System.out.println(flight.getDate());
+
+        // testFlightList(FlightList.getFlight(flight.getID()));
     }
 
     
