ac3081a8e3bb7af39789ca46142186a8b56a00bc
tonio64286 <tonio64286@gmail.com>
2022-03-28 21:57:54 -0400

    UITest.java added + more FlightSystem progress

--- src/FlightSystem.java
+++ src/FlightSystem.java
@@ -15,15 +15,12 @@ import java.util.ArrayList;
  * @author Chris Moore, Lyn Cork, Evan Scales
  */
 public class FlightSystem {
-  private FlightList flightList;
   private Scanner scanner = new Scanner(System.in);
-  private UserList userList;
+  private UserList userList = UserList.getInstance();
   private HotelList hotelList;
-  private User user;
-  private Flight flight;
+  private FlightList flightList;
   DateTimeFormatter format = DateTimeFormatter.ofPattern("MM-dd-yyyy");
   
-
 /**
  * Default constructor for FlightSystem
  */
@@ -52,6 +49,7 @@ public class FlightSystem {
       return user;
     }
     else {
+      System.out.println("Incorrect username or password. Please try again.");
       return null;
     }
   }
@@ -78,9 +76,8 @@ public class FlightSystem {
     ArrayList<Ticket> tickets = new ArrayList<>();
     ArrayList<HotelReservation> hotelReservations = new ArrayList<>();
     ArrayList<Friend> friends = new ArrayList<>();
-
     // NAME + AGE
-    System.out.println("----- Enter the Following Information -----\n" +
+    System.out.println("----- Enter the Following Information -----\n\n" +
     "----- Your Name -----\n"+
     "First name: ");
     firstName = scanner.nextLine();
@@ -88,7 +85,6 @@ public class FlightSystem {
     lastName = scanner.nextLine();
     System.out.println("Age: ");
     age = scanner.nextInt();
-
     // GENDER
     System.out.println("----- Your Gender -----\n"+
     "1. Male\n"+
@@ -109,10 +105,9 @@ public class FlightSystem {
       default: gender = "Undisclosed";
       break;
     }
-
     // ADDRESS
-    System.out.println("----- Your Address -----\n"+
-    "Country: ");
+    System.out.println("----- Your Address -----");
+    System.out.println("Country:");
     String country = scanner.nextLine();
     System.out.println("Street: ");
     String street = scanner.nextLine();
@@ -123,12 +118,10 @@ public class FlightSystem {
     System.out.println("Postal Code: ");
     String postal = scanner.nextLine();
     address = street + ", " + city + ", " + state + ", " + country + ", " + postal;
-
     // EMAIL
     System.out.println("----- Your Email -----\n"+
     "Email: ");
     email = scanner.nextLine();
-
     // PASSWORD
     System.out.println("----- Create Your Password -----");
     boolean noMatch = true;
@@ -147,14 +140,16 @@ public class FlightSystem {
         System.out.println("Passwords do not match. Please try again.");
       }
     }
-
     // Create account with user-input attributes, 
     // with default values where applicable
-    new RegisteredUser(firstName, lastName, email, age, address, password, gender, 
+    RegisteredUser saveThis = new RegisteredUser(firstName, lastName, email, age, address, password, gender, 
     preferredAirport, frequentFlyer, admin, medicalCondition, blackListedAirports, 
     tickets, hotelReservations, friends);
-    System.out.println("Account succesfully created. You will now be prompted to log in.");
+    //Write saveThis to Users.json 
+    System.out.println("Account succesfully created. You can now log in.");
   }
+
+
 
 /**
  * Allows the user to view and edit
@@ -162,46 +157,234 @@ public class FlightSystem {
  * tickets, reservations, preferences, and friends.
  */
   public void manageAccount(RegisteredUser user) {
-    int select = scanner.nextInt();
-    System.out.println("----- My Account -----");
-    String isFF = "not ";
-    if(user.getFrequentFlyer()) {
-      isFF = "";
+    while(true){
+      String isFF = "not ";
+      if(user.getFrequentFlyer()) {
+        isFF = "";
+      }
+      System.out.println("----- My Account -----");
+      System.out.println("You are " + isFF + "a frequent flyer.\n\n" +
+      "1. View Plane Tickets\n2. View Hotel Reservations\n3. View Preferences\n" +
+      "4. View Linked People Options\n5. Return to Menu\n\n" +"What would you like to do?: ");
+      int mainSelect = scanner.nextInt();
+      boolean endLoop = false;
+      switch(mainSelect) {
+        case 1: viewTickets(user);
+          break;
+        case 2: viewReservations(user);
+          break;
+        case 3: viewPreferences(user);
+          break;
+        case 4: viewLinkedPeople(user);
+          break;
+        case 5: break;
+        default: System.out.println("Invalid input, please try again!");
+          break;
+      }
+      if(mainSelect == 5) {
+        break;
+      }
+      else {
+        continue;
+      }
     }
-    System.out.println("You are " + isFF + "a frequent flyer.\n\n" +
-    "1. View Plane Tickets\n2. View Hotel Reservations\n3. View Preferences\n" +
-    "4. View Linked People Options\n5. Return to Menu\n\n" +"What would you like to do?: ");
-    boolean loop = true;
-    while(loop) {
-    switch(select) {
-      case 1: viewTickets(user);
-      break;
-      case 2: viewReservations(user);
-      break;
-      case 3: viewPreferences(user);
-      break;
-      case 4: viewLinkedPeople(user);
-      break;
-      case 5: break;
-      default: System.out.println("Invalid input, please try again!");
-    }
-  }
   }
 
+  /**
+   * Sub-method of ManageAccount(), allows user to view plane tickets
+   * @param user Current RegisteredUser instance
+   */
   public void viewTickets(RegisteredUser user) {
-    System.out.println(user.getTickets());
+    while(true) {
+      System.out.println("----- Plane Tickets -----");
+      ArrayList<Ticket> tickets = user.getTickets();
+      for(int i = 0; i < tickets.size(); i++) {
+        System.out.println(i+1 + ". " + tickets.get(i));
+      }
+      System.out.println("Which ticket would you like to view? (Enter 0 to return to menu):");
+      int view = scanner.nextInt();
+      if(view == 0) {
+        manageAccount(user);
+      }
+      else {
+        tickets.get(view-1).toString();
+      }
+      System.out.println("1. Cancel ticket and request refund\n2. Add checked baggage\n" + 
+      "3. Return to menu\nWhat would you like to do?:");
+      int select = scanner.nextInt();
+      switch(select) {
+        case 1: tickets.remove(view-1);
+        System.out.println("Your ticket has been cancelled. A refund will be issued to your account.");
+        manageAccount(user);
+        break;
+        case 2: System.out.println("----- Enter the Following Information -----\n Weight of bag (lbs):");
+        double weight = scanner.nextDouble();
+        // Is there anywhere to store a bags' weight??
+        tickets.get(view-1).setNumOfCheckedBags(tickets.get(view-1).getNumOfCheckedBags() + 1);
+        System.out.println("Success. You have been charged $30");
+        case 3: manageAccount(user);
+        break;
+        default: break;
+      }
+      continue;
+    }
   }
 
+  /**
+   * Sub-method of ManageAccount(), allows user to view hotel reservations
+   * @param user Current RegisteredUser instance
+   */
   public void viewReservations(RegisteredUser user) {
-    System.out.println(user.getHotelReservations());
+    while(true){
+      System.out.println("----- Hotel Reservations -----");
+      ArrayList<HotelReservation> reservations = user.getHotelReservations();
+      for(int i = 0; i < reservations.size(); i++) {
+        System.out.println(i+1 + ". " + reservations.get(i));
+      }
+      System.out.println("Which reservation would you like to view? (Enter 0 to return to menu):");
+      int view = scanner.nextInt();
+      if(view == 0) {
+        manageAccount(user);
+      }
+      else {
+        reservations.get(view-1).toString();
+      }
+      System.out.println("1. Cancel reservation and request refund\n2. Return to menu:\n" +
+      "What would you like to do?:");
+      int select = scanner.nextInt();
+      switch(select) {
+        case 1: reservations.remove(view-1);
+        System.out.println("Your reservation has been cancelled. A refund will be issued to your account.");
+        manageAccount(user);
+        break;
+        case 2: manageAccount(user);
+        break;
+        default: break;
+      }
+      continue;
+    }
   }
-
+  
+  /**
+   * Sub-method of ManageAccount(), allows user to change certain preferences
+   * @param user Current RegisteredUser instance
+   */
   public void viewPreferences(RegisteredUser user) {
-    
+      System.out.println("----- Preferences -----\n"+
+      "1. Edit medical condition\n2. Change preferred airport\n3. Add blacklisted airport \n4. Return to my account\n\n"+
+      "What would you like to do?");
+      int select = scanner.nextInt();
+      scanner.nextLine();
+      switch(select) {
+        case 1: System.out.println("Do you have a disability or other medical condition which you would like airlines and hotels to accomodate? (y/n):");
+        String medical = scanner.nextLine();
+        switch(medical) {
+          case "y": user.setMedicalCondition(true);
+            break;
+          case "n": user.setMedicalCondition(false);
+            break;
+        }
+        System.out.println("Medical condition updated.");
+        break;
+        case 2: System.out.println("Please set your preferred airport (Use three-letter code, ex. LAX):");
+          String preferredAirport = scanner.nextLine();
+          user.setPreferredAirport(preferredAirport);
+          System.out.println("Preferred airport updated.");
+        break;
+        case 3: System.out.println("Please add a blacklisted airport (Use three-letter code, ex. LAX):");
+          String blacklistedAirport = scanner.nextLine();
+          user.addBlackListedAirport(blacklistedAirport);
+          System.out.println("Blacklisted airports updated.");
+        break;
+        case 4: manageAccount(user);
+        break;
+        default: break;
+      }
   }
 
+  /**
+   * 
+   * @param user
+   */
   public void viewLinkedPeople(RegisteredUser user) {
-    
+    System.out.println("----- Linked People Options -----\n"+
+    "1. View linked people\n2. Add people\n3. Return to my account\n\n"+
+    "What would you like to do?");
+    int select = scanner.nextInt();
+    scanner.nextLine();
+    switch(select) {
+      case 1: System.out.println("----- Linked People -----\n");
+        for(int i = 0; i < user.getFriends().size(); i++) {
+          System.out.println((i+1) + ". " + user.getFriends().get(i));
+        }
+        break;
+      case 2: String firstName;
+      String lastName;
+      String email;
+      int age;
+      String address;
+      String gender;
+      boolean medicalCondition = false;
+      // NAME + AGE
+      System.out.println("----- Enter the Following Information -----\n\n" +
+      "----- Friend's Name -----\n"+
+      "First name: ");
+      firstName = scanner.nextLine();
+      System.out.println("Last name: ");
+      lastName = scanner.nextLine();
+      System.out.println("Age: ");
+      age = scanner.nextInt();
+      scanner.nextLine();
+      // GENDER
+      System.out.println("----- Friend's Gender -----\n"+
+      "1. Male\n"+
+      "2. Female\n"+
+      "3. Unspecified\n"+
+      "4. Undisclosed\n\n"+
+      "Select your gender: ");
+      int genderSelect = scanner.nextInt();
+      switch(genderSelect) {
+        case 1: gender = "Male";
+        break;
+        case 2: gender = "Female";
+        break;
+        case 3: gender = "Unspecified";
+        break;
+        case 4: gender = "Undisclosed";
+        break;
+        default: gender = "Undisclosed";
+        break;
+      }
+      // ADDRESS
+      System.out.println("----- Friend's Address -----");
+      System.out.println("Country:");
+      String country = scanner.nextLine();
+      System.out.println("Street: ");
+      String street = scanner.nextLine();
+      System.out.println("City: ");
+      String city = scanner.nextLine();
+      System.out.println("State: ");
+      String state = scanner.nextLine();
+      System.out.println("Postal Code: ");
+      String postal = scanner.nextLine();
+      address = street + ", " + city + ", " + state + ", " + country + ", " + postal;
+      // EMAIL
+      System.out.println("----- Friend's Email -----\n"+
+      "Email: ");
+      email = scanner.nextLine();
+      // MEDICAL CONDITION
+      System.out.println("Does this person have a medical condition? (y/n)\n");
+      String medical = scanner.nextLine();
+      switch(medical) {
+        case "y": medicalCondition = true;
+        case "n": medicalCondition = false;
+      }
+      // Create new friend, and add to user's friend list
+      Friend friend = new Friend(firstName, lastName, age, medicalCondition, gender, email, address);
+      user.addFriend(friend);
+    case 3: manageAccount(user);
+      break;
+      }
   }
 
 /**
@@ -214,11 +397,10 @@ public class FlightSystem {
   }
 
 /**
- * OVERRIDDEN FUNCTION FOR NONREGISTERED USERS
  * Allows the user to search for flights by certain parameters
- * Prompts the user to create an account before booking a flight
+ * Prompts non-registered users to create an account before booking a flight
  */
-  public void searchForFlights() {
+  public void searchForFlights(RegisteredUser user) {
     System.out.println("----- Enter the Following Information -----\n" +
     "----- Flight Type -----\n" +
     "1. One way\n"+
@@ -255,7 +437,6 @@ public class FlightSystem {
       int cabinChoice = scanner.nextInt();
 
       // SECOND FLIGHT (ROUND-TRIP)
-
       System.out.println("Which flight would you like to choose?: ");
       int flightChoiceTwo = scanner.nextInt();
   
@@ -275,19 +456,10 @@ public class FlightSystem {
   }
 
 /**
- * OVERRIDDEN FUNCTION FOR REGISTERED USERS
- * Allows the user to search for flights by certain parameters
- */
-public void searchForFlights(RegisteredUser user) {
-    
-}
-
-/**
- * OVERRIDDEN FUNCTION FOR NONREGISTERED USERS
  * Allows the user to search for hotels by certain parameters
- * Prompts the user to create an account before booking a hotel
+ * Prompts non-registered users to create an account before booking a hotel
  */
-  public void searchForHotels() {
+  public void searchForHotels(RegisteredUser user) {
     System.out.println("----- Enter the Following Information ----- ");
 
     System.out.println("----- Hotel City ----- ");
@@ -305,14 +477,6 @@ public void searchForFlights(RegisteredUser user) {
     System.out.println("----- Hotel Booking ----- ");
     
   }
-
-/**
- * OVERRIDDEN FUNCTION FOR REGISTERED USERS
- * Allows the user to search for hotels by certain parameters
- */
-public void searchForHotels(RegisteredUser user) {
-    
-}
 
 /* 
 ===================
--- src/FlightSystemUI.java
+++ src/FlightSystemUI.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * FlightSystemUI
@@ -9,69 +10,101 @@ import java.util.Scanner;
  */
 public class FlightSystemUI {
   private Scanner scanner = new Scanner(System.in);
-  private FlightSystem system;
-  private RegisteredUser currentUser;
+  private FlightSystem system = new FlightSystem();
+  private RegisteredUser currentUser = new RegisteredUser();
 
 /**
  * Displays logged-out user menu by default.
+ * MENU AVAILABLE FOR DEBUGGING PURPOSES ONLY
  */
   public void run() {
-    {
+    System.out.println("Welcome to the UI Debugger. Please select the menu to test:\n" +
+    "1. Guest Menu\n2. Registered User Menu\n3. Administrator Menu");
+    int menuSelect = scanner.nextInt();
+    switch(menuSelect) {
+      case 1: generateTestUser();
       displayMenuUser();
+      break;
+      case 2: generateTestUser();
+      displayMenuRegisteredUser();
+      break;
+      case 3: generateTestUser();
+      currentUser.setAdmin(true);
+      displayMenuAdmin();
+      break;
     }
+  }
+
+  /**
+   * Creates an example user for testing purposes
+   */
+  private void generateTestUser() {
+    ArrayList<String> blackListedAirports = new ArrayList<>();
+    ArrayList<Ticket> tickets = new ArrayList<>();
+    ArrayList<HotelReservation> hotelReservations = new ArrayList<>();
+    ArrayList<Friend> friends = new ArrayList<>();
+    RegisteredUser testUser = new RegisteredUser("John", "Doe", "johndoe@gmail.com", 41, "123 Fake Street, SC, USA, 29201", "password", "Male", 
+    "LAX", true, false, false, blackListedAirports, 
+    tickets, hotelReservations, friends);
+    currentUser = testUser;
   }
 
 /**
  * Initial display for unregistered (or logged-out) users.
  */
-  private void displayMenuUser() {
-    System.out.println("Welcome to our Flight and Hotel Booker!\n" + 
-    "You are not logged in.\n" +
-    "************ Main Menu ************\n" +
-    "1. Search Flights\n2. Search Hotels\n3. Create Account\n4. Login\n" +
-    "What would you like to do?:");
-    boolean loop = true;
-    int select = scanner.nextInt();
-    switch(select) {
-      case 1: system.searchForFlights();
-      break;
-      case 2: system.searchForHotels();
-      break;
-      case 3: system.createAccount();
-              while(loop) {
-                currentUser = system.login();
-                if (currentUser != null) {
-                  loop = false;
-                  if(currentUser.getAdmin()) {
-                    displayMenuAdmin();
+  public void displayMenuUser() {
+    while(true){
+      System.out.println("Welcome to our Flight and Hotel Booker!\n" + 
+      "You are not logged in.\n\n" +
+      "************ Main Menu ************\n" +
+      "1. Search Flights\n2. Search Hotels\n3. Create Account\n4. Login\n5. Exit\n\n" +
+      "What would you like to do?:");
+      boolean loop = true;
+      int select = scanner.nextInt();
+      switch(select) {
+        case 1: system.searchForFlights(currentUser);
+        break;
+        case 2: system.searchForHotels(currentUser);
+        break;
+        case 3: system.createAccount();
+                while(loop) {
+                  currentUser = system.login();
+                  if (currentUser != null) {
+                    loop = false;
+                    if(currentUser.getAdmin()) {
+                      displayMenuAdmin();
+                    }
+                    else {
+                      displayMenuRegisteredUser();
+                    }
                   }
                   else {
-                    displayMenuRegisteredUser();
+                    System.out.println("Login failed, please try again!");
                   }
                 }
-                else {
-                  System.out.println("Login failed, please try again!");
-                }
-              }
-              break;
-      case 4: while(loop) {
-                currentUser = system.login();
-                if (currentUser != null) {
-                  loop = false;
-                  if(currentUser.getAdmin()) {
-                    displayMenuAdmin();
+                break;
+        case 4: while(loop) {
+                  currentUser = system.login();
+                  if (currentUser != null) {
+                    loop = false;
+                    if(currentUser.getAdmin()) {
+                      displayMenuAdmin();
+                    }
+                    else {
+                      displayMenuRegisteredUser();
+                    }
                   }
                   else {
-                    displayMenuRegisteredUser();
+                    System.out.println("Login failed, please try again!\n");
                   }
                 }
-                else {
-                  System.out.println("Login failed, please try again!");
-                }
-              }
-              break;
-      default: System.out.println("Invalid input, please try again!");
-      break;
+                break;
+        case 5: system.logout();
+        break;
+        default: System.out.println("Invalid input, please try again!\n");
+        break;
+      }
+      continue;
     }
   }
 
@@ -79,23 +112,26 @@ public class FlightSystemUI {
  * Display menu for registered users, once they have logged in.
  */
   private void displayMenuRegisteredUser() {
-    int select = scanner.nextInt();
-    System.out.println("Welcome to our Flight and Hotel Booker!\n" +
-    "You are logged in as a regular user.\n" +
-    "************ Main Menu ************\n" +
-    "1. Search Flights\n2. Search Hotels\n3. Manage Account\n4. Logout\n" +
-    "What would you like to do?: ");
-    switch(select) {
-      case 1: system.searchForFlights(currentUser);
-      break;
-      case 2: system.searchForHotels(currentUser);
-      break;
-      case 3: system.manageAccount(currentUser);
-      break;
-      case 4: system.logout();
-      break;
-      default: System.out.println("Invalid input, please try again!");
-      break;
+    while(true) {
+      System.out.println("Welcome to our Flight and Hotel Booker!\n" +
+      "Hello, " + currentUser.getFirstName() + " " + currentUser.getLastName() + ". You are logged in as a regular user.\n\n" +
+      "************ Main Menu ************\n" +
+      "1. Search Flights\n2. Search Hotels\n3. Manage Account\n4. Logout\n\n" +
+      "What would you like to do?: ");
+      int select = scanner.nextInt();
+      switch(select) {
+        case 1: system.searchForFlights(currentUser);
+        break;
+        case 2: system.searchForHotels(currentUser);
+        break;
+        case 3: system.manageAccount(currentUser);
+        break;
+        case 4: system.logout();
+        break;
+        default: System.out.println("Invalid input, please try again!\n");
+        break;
+      }
+      continue;
     }
   }
 
@@ -103,37 +139,43 @@ public class FlightSystemUI {
  * Display menu for administrators, once they have logged in.
  */
   private void displayMenuAdmin() {
-    
-    int select = scanner.nextInt();
-    System.out.println("Welcome to our Flight and Hotel Booker!\n"+
-    "You are logged in as an administrator.\n" +
-    "************ Main Menu ************\n" +
-    "1. Search Flights\n2. Search Hotels\n3. Manage Account\n4. Add Flight\n"  +
-    "5. Edit Flight\n6.Remove Flight\n7.Add Hotel\n8.Edit Hotel\n" +
-    "9. Remove Hotel\n0. Logout\nWhat would you like to do?: ");
-    switch(select) {
-      case 1: system.searchForFlights(currentUser);
-      break;
-      case 2: system.searchForHotels(currentUser);
-      break;
-      case 3: system.manageAccount(currentUser);
-      break;
-      case 4: system.addFlight();
-      break;
-      case 5: system.editFlight();
-      break;
-      case 6: system.removeFlight();
-      break;
-      case 7: system.addHotel();
-      break;
-      case 8: system.editHotel();
-      break;
-      case 9: system.removeHotel();
-      break;
-      case 0: system.logout();
-      break;
-      default: System.out.println("Invalid input, please try again!");
-      break;
+    while(true) {
+      if(currentUser.getAdmin() != true) {
+        System.out.println("<ERROR>: You are not signed in as an administrator. Logging out...");
+        System.exit(0);
+      }
+      System.out.println("Welcome to our Flight and Hotel Booker!\n"+
+      "Hello, " + currentUser.getFirstName() + " " + currentUser.getLastName() + ". You are logged in as an administrator.\n\n" +
+      "************ Main Menu ************\n" +
+      "1. Search Flights\n2. Search Hotels\n3. Manage Account\n4. Add Flight\n"  +
+      "5. Edit Flight\n6. Remove Flight\n7. Add Hotel\n8. Edit Hotel\n" +
+      "9. Remove Hotel\n0. Logout\n\nWhat would you like to do?: ");
+      int select = scanner.nextInt();
+      switch(select) {
+        case 1: system.searchForFlights(currentUser);
+        break;
+        case 2: system.searchForHotels(currentUser);
+        break;
+        case 3: system.manageAccount(currentUser);
+        break;
+        case 4: system.addFlight();
+        break;
+        case 5: system.editFlight();
+        break;
+        case 6: system.removeFlight();
+        break;
+        case 7: system.addHotel();
+        break;
+        case 8: system.editHotel();
+        break;
+        case 9: system.removeHotel();
+        break;
+        case 0: system.logout();
+        break;
+        default: System.out.println("Invalid input, please try again!\n");
+        break;
+      }
+      continue;
     }
   }
 }
