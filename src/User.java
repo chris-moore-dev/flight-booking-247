e6621253157c481a77e1cce91d42b7342fa30c61
finn-cork <fcork@email.sc.edu>
2022-03-28 23:41:17 -0400

    Finished all but 2 functions in User

--- src/User.java
+++ src/User.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Map.Entry;
 
 /**
  * User class
@@ -78,11 +79,35 @@ public class User {
   }
 
   /**
-   * 
-   * @param flights
+   * Takes an ArrayList of flights and sorts it from earliest landing time to latest landing time
+   * @param flights the passed ArrayList of flights
    */
   public void filterFlightsByArrivalTimeEarlyLate(ArrayList<Flight> flights) {
-
+    int n = flights.size();
+    for(int i = 0; i < n-1; i++) {
+      for(int j = 0; j < n-i-1; j++) {
+        Flight flightOne = flights.get(j);
+        Flight flightTwo = flights.get(j+1);
+        String timeStrOne = flightOne.getLandingTime();
+        String timeStrTwo = flightTwo.getLandingTime();
+        if (timeStrOne.contains("AM") && timeStrOne.contains("PM")) {
+          continue;
+        }
+        else if (timeStrOne.contains("PM") && timeStrTwo.contains("AM")) {
+          flights.add(j, flightTwo);
+          flights.add(j+1, flightOne);
+        } else {
+          timeStrOne = timeStrOne.replaceAll("[^0-9]", "");
+          timeStrTwo = timeStrTwo.replaceAll("[^0-9]", "");
+          int timeOne = Integer.parseInt(timeStrOne);
+          int timeTwo = Integer.parseInt(timeStrTwo);
+          if (timeOne > timeTwo) {
+            flights.add(j, flightTwo);
+            flights.add(j+1, flightOne);
+          }
+        }
+      }
+    }
   }
 
   /**
@@ -94,11 +119,35 @@ public class User {
   }
 
   /**
-   * 
-   * @param flights
+   * Takes an ArrayList of flights and sorts it from earliest departure time to latest departure time
+   * @param flights passed ArrayList of flights
    */
   public void filterFlightsByDepartingTimeEarlyLate(ArrayList<Flight> flights) {
-
+    int n = flights.size();
+    for(int i = 0; i < n-1; i++) {
+      for(int j = 0; j < n-i-1; j++) {
+        Flight flightOne = flights.get(j);
+        Flight flightTwo = flights.get(j+1);
+        String timeStrOne = flightOne.getTakeOffTime();
+        String timeStrTwo = flightTwo.getTakeOffTime();
+        if (timeStrOne.contains("AM") && timeStrOne.contains("PM")) {
+          continue;
+        }
+        else if (timeStrOne.contains("PM") && timeStrTwo.contains("AM")) {
+          flights.add(j, flightTwo);
+          flights.add(j+1, flightOne);
+        } else {
+          timeStrOne = timeStrOne.replaceAll("[^0-9]", "");
+          timeStrTwo = timeStrTwo.replaceAll("[^0-9]", "");
+          int timeOne = Integer.parseInt(timeStrOne);
+          int timeTwo = Integer.parseInt(timeStrTwo);
+          if (timeOne > timeTwo) {
+            flights.add(j, flightTwo);
+            flights.add(j+1, flightOne);
+          }
+        }
+      }
+    }
   }
 
   /**
@@ -110,11 +159,27 @@ public class User {
   }
 
   /**
-   * 
-   * @param flights
+   * Takes an ArrayList of flights and sorts it from least to greatest total flight time
+   * @param flights passed Arraylist of flights
    */
   public void filterFlightsByTotalFlightTime(ArrayList<Flight> flights) {
-
+    int n = flights.size();
+    for(int i = 0; i < n-1; i++) {
+      for(int j = 0; j < n-i-1; j++) {
+        Flight flightOne = flights.get(j);
+        Flight flightTwo = flights.get(j+1);
+        String timeStrOne = flightOne.getTotalFlightTime();
+        timeStrOne = timeStrOne.replaceAll("[^0-9]", "");
+        String timeStrTwo = flightTwo.getTotalFlightTime();
+        timeStrTwo = timeStrTwo.replaceAll("[^0-9]", "");
+        int timeOne = Integer.parseInt(timeStrOne);
+        int timeTwo = Integer.parseInt(timeStrTwo);
+        if (timeOne > timeTwo) {
+          flights.add(j, flightTwo);
+          flights.add(j+1, flightOne);
+        }
+      }
+    }
   }
 
   /**
@@ -168,54 +233,121 @@ public class User {
   }
 
   /**
-   * 
-   * @param closestAirport
-   * @return
+   * Returns an ArrayList of hotels that match a passed closest airport
+   * @param closestAirport the passed airport
+   * @return ArrayList of hotels
    */
   public ArrayList<Hotel> getHotelsByAirport(String closestAirport) {
-    
+    HotelList hotelList = HotelList.getInstance();
+    ArrayList<Hotel> hotels = hotelList.getHotels();
+    ArrayList<Hotel> ret = new ArrayList<Hotel>();
+    for (int i = 0; i < hotels.size(); i++) {
+      Hotel hotel = hotels.get(i);
+      String airport = hotel.getClosestAirport();
+      if (airport == closestAirport) {
+        ret.add(hotel);
+      }
+    }
+    return ret;
   }
 
   /**
-   * 
-   * @param city
-   * @return
+   * Returns an ArrayList of hotels that match a passed city
+   * @param city the passed city
+   * @return ArrayList of hotels
    */
   public ArrayList<Hotel> getHotelsByCity(String city) {
-
+    HotelList hotelList = HotelList.getInstance();
+    ArrayList<Hotel> hotels = hotelList.getHotels();
+    ArrayList<Hotel> ret = new ArrayList<Hotel>();
+    for (int i = 0; i < hotels.size(); i++) {
+      Hotel hotel = hotels.get(i);
+      String searchCity = hotel.getCity();
+      if (searchCity == city) {
+        ret.add(hotel);
+      }
+    }
+    return ret;
   }
 
   /**
-   * 
-   * @param closestAirport
-   * @param name
-   * @return
+   * Returns an ArrayList of hotels that match a passed airport and name
+   * @param closestAirport the passed airport
+   * @param name name of the hotel
+   * @return ArrayList of hotels
    */
   public ArrayList<Hotel> getHotels(String closestAirport, String name) {
-
+    HotelList hotelList = HotelList.getInstance();
+    ArrayList<Hotel> hotels = hotelList.getHotels();
+    ArrayList<Hotel> ret = new ArrayList<Hotel>();
+    for (int i = 0; i < hotels.size(); i++) {
+      Hotel hotel = hotels.get(i);
+      String airport = hotel.getClosestAirport();
+      String searchName = hotel.getCompany();
+      if (airport == closestAirport && name == searchName) {
+        ret.add(hotel);
+      }
+    }
+    return ret;
   }
 
   /**
-   * 
-   * @param hotels
+   * Takes an ArrayList of hotels and an airport, and filters ArrayList to only hotels with that closest airport
+   * @param hotels passed ArrayList of hotels
+   * @param closestAirport passed airport
    */
-  public void filterHotelsByClosenessToAirport(ArrayList<Hotel> hotels) {
-    
+  public void filterHotelsByClosenessToAirport(ArrayList<Hotel> hotels, String closestAirport) {
+    ArrayList<Hotel> temp = new ArrayList<Hotel>();
+    for (int i = 0; i < hotels.size(); i++) {
+      Hotel hotel = hotels.get(i);
+      String airport = hotel.getClosestAirport();
+      if (airport == closestAirport) {
+        temp.add(hotel);
+      }
+    }
+    hotels = temp;
   }
 
   /**
-   * 
-   * @param ammenities
-   * @param hotels
+   * Takes an ArrayList of hotels and an ArrayList of amenities, and filters ArrayList to only hotels with those amenities
+   * @param amenities passed ArrayList of amenitites
+   * @param hotels passed ArrayList of hotels
    */
-  public void filterHotelsByAmmenities(ArrayList<String> ammenities, ArrayList<Hotel> hotels) {
-    
+  public void filterHotelsByAmmenities(ArrayList<String> amenities, ArrayList<Hotel> hotels) {
+    ArrayList<Hotel> temp = new ArrayList<Hotel>();
+    for (int i = 0; i < hotels.size(); i++) {
+      Hotel hotel = hotels.get(i);
+      ArrayList<String> amenity = hotel.getAmenities();
+      if (amenity.containsAll(amenities)) {
+        temp.add(hotel);
+      }
+    }
+    hotels = temp;
   }
 
   /**
-   * 
-   * @param hotels
+   * Sorts passed hotel ArrayList from least to greatest average price
+   * @param hotels arraylist of hotels
    */
   public void filterHotelsByPrice(ArrayList<Hotel> hotels) {
+    int n = hotels.size();
+    int averageOne = 0;
+    int averageTwo = 0;
+    for(int i = 0; i < n-1; i++) {
+      for(int j = 0; j < n-i-1; j++) {
+        Hotel hotelOne = hotels.get(j);
+        for (Entry<String, Integer> entry : hotelOne.getPricing().entrySet()) {
+          averageOne += entry.getValue();
+        }
+        Hotel hotelTwo = hotels.get(j+1);
+        for (Entry<String, Integer> entry : hotelTwo.getPricing().entrySet()) {
+          averageTwo += entry.getValue();
+        }
+        if (averageOne > averageTwo) {
+          hotels.add(j, hotelTwo);
+          hotels.add(j+1, hotelOne);
+        }
+      }
+    }
   }
 }
