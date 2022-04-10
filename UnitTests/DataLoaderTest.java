e35fc2ef05f68f9f1e749e562f43ad625378cf2e
Evan Scales <evan.m.scales@gmail.com>
2022-04-10 17:37:28 -0400

    fixed testing bug

--- UnitTests/DataLoaderTest.java
+++ UnitTests/DataLoaderTest.java
@@ -17,11 +17,32 @@ public class DataLoaderTest {
 
     @BeforeEach
     public void setup() {
-
+        UserList.getInstance().getUsers().clear();
+        FlightList.getInstance().getFlights().clear();
+        HotelList.getInstance().getHotels().clear();
+        DataWriter.saveDatabase();
     }
 
     @AfterEach
     public void tearDown() {
+        UserList.getInstance().getUsers().clear();
+        FlightList.getInstance().getFlights().clear();
+        HotelList.getInstance().getHotels().clear();
+        DataWriter.saveDatabase();
+    }
 
+    @Test
+    public void testGetFlights() {
+
+    }
+
+    @Test
+    public void testGetUsers() {
+        
+    }
+
+    @Test
+    public void testGetHotels() {
+        
     }
 }
