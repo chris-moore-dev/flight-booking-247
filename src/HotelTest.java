4516ed475944a103a492834473d0f9ffcd6e74df
tonio64286 <tonio64286@gmail.com>
2022-04-10 15:34:54 -0400

    Prototypes for unit tests

---
+++ src/HotelTest.java
@@ -0,0 +1,28 @@
+/**
+ * This class contains the test cases for the class Hotel
+ * @author Chris Moore
+ */
+public class HotelTest {
+  
+  /**
+   * Calls all test cases for class Hotel
+   * @return true if all test cases were successful
+   */
+  public boolean test() {
+    boolean all = false;
+    //TestFlight1()
+    //TestFlight2()
+    if(all) {
+      System.out.println("All test cases were successful.");
+      return true;
+    }
+    else {
+      System.out.println("The following test cases failed:");
+    }
+    return false;
+  }
+
+  // TEST CASES
+
+  
+}
